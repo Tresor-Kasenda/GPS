0fd697e081af5de19ca2b6b036e167b8e27c06bc
tresor kasenda <tresorkasendat@gmail.com>
2024-04-23 17:19:40 +0200

    feats: create Direction - seeder - model - migration - factory

---
+++ database/factories/DirectionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Direction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Direction>
+ */
+class DirectionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'priority' => Str::random(5),
+            'abbreviation' => $this->faker->word(),
+            'designation' => $this->faker->realText(),
+        ];
+    }
+}
---
+++ database/seeders/DirectionSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\Direction;
+use Illuminate\Database\Seeder;
+
+class DirectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Direction::query()->create([
+            'priority' => 'D1',
+            'abbreviation' => "D1",
+            'designation' => "Directeur generale"
+        ]);
+    }
+}
