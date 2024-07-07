34af5608be3f26f2ba9d2bcea5d0d2aaef0a3f71
tresor kasenda <tresorkasendat@gmail.com>
2024-07-07 11:09:23 +0200

    feat (auth): use livewire to login and email verification

---
+++ app/Livewire/Pages/Auth/Login.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire\Pages\Auth;
+
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+#[Layout('layouts.guest')]
+#[Title('Authentification')]
+final class Login extends Component
+{
+    #[Validate('required|string|email')]
+    public string $email = '';
+
+    #[Validate('required|string')]
+    public string $password = '';
+
+    public function render(): View
+    {
+        return view('livewire.pages.auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function login(): void
+    {
+        $this->validate();
+
+        $this->authenticate();
+
+        Session::regenerate();
+
+        $this->redirectIntended(default: route('dashboard', absolute: false));
+    }
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @throws ValidationException
+     */
+    public function authenticate(): void
+    {
+        $this->ensureIsNotRateLimited();
+
+        if (!Auth::attempt($this->only(['email', 'password']))) {
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+                'form.email' => trans('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    /**
+     * Ensure the authentication request is not rate limited.
+     */
+    protected function ensureIsNotRateLimited(): void
+    {
+        if (!RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        event(new Lockout(request()));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'form.email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    /**
+     * Get the authentication rate limiting throttle key.
+     */
+    protected function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->email) . '|' . request()->ip());
+    }
+}
---
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -0,0 +1,57 @@
+<div>
+    <div class="nk-block-head">
+        <div class="nk-block-head-content">
+            <h4 class="nk-block-title text-center">{{ __("S'authentifier") }}</h4>
+            <div class="nk-block-des">
+                <p>
+                    {{ __("Accédez a GRH en utilisant votre email et votre code d'accès.") }}
+                </p>
+            </div>
+        </div>
+    </div>
+    <form wire:submit.prevent="login">
+        <div class="form-group">
+            <div class="form-label-group">
+                <x-ui.forms.input.label for="email" :value="__('Email')"/>
+            </div>
+            <div class="form-control-wrap">
+                <x-ui.forms.input
+                    id="email"
+                    class="form-control-lg"
+                    type="email"
+                    name="email"
+                    wire:model="email"
+                    :value="old('email')"
+                    required
+                    autofocus
+                    autocomplete="username"
+                    placeholder="Enter your email address or username"
+                />
+                <x-ui.forms.input.error :messages="$errors->get('email')" class="mt-2"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="form-label-group">
+                <x-ui.forms.input.label for="password" :value="__('Password')"/>
+            </div>
+            <div class="form-control-wrap">
+                <a href="#" class="form-icon form-icon-right passcode-switch lg" data-target="password">
+                    <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                    <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                </a>
+                <x-ui.forms.input
+                    id="password" class="form-control-lg"
+                    type="password"
+                    wire:model="password"
+                    name="password"
+                    required autocomplete="current-password"
+                />
+                <x-ui.forms.input.error :messages="$errors->get('password')" class="mt-2"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <x-ui.block.button.submit class="btn-lg btn-dim btn-block" :action="__('Authentifier')"/>
+        </div>
+    </form>
+</div>
