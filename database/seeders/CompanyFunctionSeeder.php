6485f005c105733d232093ade8b5406368e5517c
tresor kasenda <tresorkasendat@gmail.com>
2024-08-26 17:43:11 +0200

    feat (company): add level and company

---
+++ database/seeders/CompanyFunctionSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CompanyFunctionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
